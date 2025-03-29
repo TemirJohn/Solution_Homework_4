8b72d3548e8f4dcd79da588cf15ed772db237f48
TemirJohn <timur.dzhuldasov@outlook.com>
2025-03-29 11:59:33 +0500

    update

--- src/main/java/org/example/Singleton/ConfigManagerDemo.java
+++ src/main/java/org/example/Singleton/ConfigManagerDemo.java
@@ -6,13 +6,14 @@ public class ConfigManagerDemo {
     public static void main(String[] args) {
         ConfigurationManager configManager = ConfigurationManager.getInstance();
 
-        System.out.println("maxPlayers: " + configManager.getConfiguration("maxPlayers"));
         configManager.printAllConfigs();
 
 
         configManager.setConfiguration("maxPlayers", "200");
         configManager.setConfiguration("defaultLanguage", "fr");
-        configManager.setConfiguration("newFeatureEnabled", "true");
+        configManager.setConfiguration("gameDifficulty", "hard");
+
+
 
         System.out.println("----------");
         configManager.printAllConfigs();
